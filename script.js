d5674b51917326e251b52de7df5a0ab61d5cb632
brendan <brendanjef@gmail.com>
2022-05-15 22:13:36 -0700

    Add compute function

--- script.js
+++ script.js
@@ -40,7 +40,29 @@ const chooseOperation = function(operation) {
 };
 
 const compute = function() {
-
+    let computation
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+        case '+':
+            computation = prev + current
+            break;
+        case '-':
+            computation = prev  - current
+            break
+        case '*':
+            computation = prev * current
+            break
+        case '÷' :
+            computation = prev / current
+            break
+        default:
+            return              
+    }
+    this.currentOperand = computation
+    this.operation = undefined
+    this.previousOperand = ''
 };
 
 const updateDisplay = function() {
